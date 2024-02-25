5996fcb22fc004e1c8f9a030c2a5d336c3cf98ac
MeisterMich <126253388+MeisterMich@users.noreply.github.com>
2024-02-25 20:50:29 +0300

    Добавлено:
    1) Реализована логика main()

--- 02_PushPip/cow_say.py
+++ 02_PushPip/cow_say.py
@@ -1,22 +1,23 @@
-import cowsay as cs
-import argparse as arg
+import cowsay
+import argparse
 
-parser = arg.ArgumentParser()
-	
-parser.add_argument("-e", "-eye_string", required=False)
-parser.add_argument("-t", "-tongue", required=False)
-parser.add_argument("-f", "-cowfile", required=False)
-parser.add_argument("-o", "-option", required=False)
-parser.add_argument("-W", "-width", required=False)
-parser.add_argument("-b", "-wrap_buble", required=False)
-parser.add_argument("-cf", "-custom_cowfile", required=False)
-parser.add_argument("-m", "-message", required=True)
+parser = argparse.ArgumentParser()
+parser.add_argument("-f", default="cow", required=False)
+parser.add_argument("-o", default="-b", required=False)
+parser.add_argument("-e", default="oo", required=False)
+parser.add_argument("-t", default="U", required=False)
+parser.add_argument("-W", default=40, required=False)
+parser.add_argument("-b", default=True, required=False)
+parser.add_argument("-cf", required=False)
+parser.add_argument("-m", required=True)
 args = parser.parse_args()
 
-
 def main():
-    pass
+    print(cowsay.cowsay(message=args.m,
+                        eyes=args.e,
+                        tongue=args.t,
+                        width=args.W,
+                        wrap_text=args.b))
 
-
-if __name__=="__main__":
+if __name__ == '__main__':
     main()
